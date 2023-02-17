ca00110e60eed939e8e272b865ad439ac22e192d
juned <princeahmad382@gmail.com>
2023-02-17 13:15:48 +0530

    an

---
+++ app/src/main/java/com/example/a1st/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.a1st
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val upload=findViewById<Button>(R.id.btnup)
+        val download=findViewById<Button>(R.id.btndown)
+        upload.setOnClickListener {
+            Toast.makeText(applicationContext,"Uploading",Toast.LENGTH_SHORT).show()
+            download.setOnClickListener{
+                Toast.makeText(applicationContext,"Downloading",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+}
